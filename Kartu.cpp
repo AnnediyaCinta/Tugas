1194f78c55723ad91c20137da853882d34c7b61f
AnnediyaCinta <104339469+AnnediyaCinta@users.noreply.github.com>
2022-08-31 13:21:01 +0700

    Update Kartu.cpp

--- Kartu.cpp
+++ Kartu.cpp
@@ -11,17 +11,23 @@ int main (){
 		cin>>bonus;
 		if (bonus == "ambil"||bonus == "AMBIL"||bonus == "Ambil") {
 			cout<<"Kartu anda = "<<kartu<<" Selamat bonus anda telah diambil"<<endl;
-		} else if (bonus == "Tukar") {
+		} else if (bonus == "Tukar"||bonus =="tukar"||bonus=="TUKAR") {
 			cout<<"Kartu anda = "<<kartu<<" Selamat bonus anda telah masuk ke poin"<<endl;
+		} else {
+			cout<<"Pilih bonus = Ambil/Tukar"<<endl;
 		}	
 	} else if (kartu == "Premium") {
 		cout<<"Pilih bonus anda Ambil/Tukar"<<endl;
 		cin>>bonus;
 		if (bonus == "ambil"||bonus == "AMBIL"||bonus == "Ambil") {
 			cout<<"Kartu anda = "<<kartu<<" Selamat bonus anda telah diambil"<<endl;
-	}	else if (bonus == "Tukar") {
+		} else if (bonus == "Tukar"||bonus =="tukar"||bonus=="TUKAR") {
 			cout<<"Kartu anda = "<<kartu<<" Selamat bonus anda telah masuk ke poin"<<endl;
-	}
+		} else {
+			cout<<"Pilih bonus = Ambil/Tukar"<<endl;
+		}
+	} else {
+		cout<<"Pilih kartu anda = Gold/Premium"<<endl;
 	}
 	
 	return 0;
